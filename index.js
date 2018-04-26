e317aeef1ddbc08af81b620b5a5c499d3c2998c2
github username <github email address>
2018-04-26 14:20:06 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function getFirstSelector(selector){
+  return  document.querySelector(selector)
+}
+
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+      let children = rankedLists[i].children
+      for (let j = 0, k = children.length; j<k; j++){
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+
+function deepestChild(){
+
+
+  const lis = document.getElementById('grand-node').querySelectorAll('div')
+    let nextNode;
+  for(let i = 0; i<lis.length; i++){
+       nextNode = lis[i]
+
+
+  }
+
+    return nextNode
+}
